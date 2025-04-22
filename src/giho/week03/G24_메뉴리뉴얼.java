aab6709369968b26a0f5a1fbf46082ace62c0831
kanggiho <kgh5554@naver.com>
2025-04-22 16:04:09 +0900

    heonwoo

--- src/giho/week03/G24_메뉴리뉴얼.java
+++ src/giho/week03/G24_메뉴리뉴얼.java
@@ -4,17 +4,60 @@ import java.util.*;
 
 public class G24_메뉴리뉴얼 {
 
+
+
+
     public String[] solution(String[] orders, int[] course) {
-        String[] answer = {};
+
+
+        ArrayList<String> answerList = new ArrayList<>();
         ArrayList<String> combinations = new ArrayList<>();
+        Map<String, Integer> comboCount = new HashMap<>();
 
         for (int i = 0; i < course.length; i++) {
             for (int j = 0; j < orders.length; j++) {
-                generateCombinations("",orders[j],course[i],0,combinations);
+                char[] chars = orders[j].toCharArray();
+                Arrays.sort(chars);
+                generateCombinations("",new String(chars) ,course[i],0,combinations);
             }
         }
+        for(int i = 0 ; i<course.length;i++){
 
-        return answer;
+
+        }
+
+
+        for (String combo : combinations) {
+            comboCount.put(combo, comboCount.getOrDefault(combo, 0) + 1);
+        }
+
+
+        for (int cour : course) {
+            int max = 0;
+            List<String> temp = new ArrayList<>();
+
+            for (Map.Entry<String, Integer> entry : comboCount.entrySet()) {
+                String combo = entry.getKey();
+                int count = entry.getValue();
+
+                if (combo.length() == cour && count >= 2) {
+                    if (count > max) {
+                        max = count;
+                        temp.clear();
+                        temp.add(combo);
+                    } else if (count == max) {
+                        temp.add(combo);
+                    }
+                }
+            }
+
+            answerList.addAll(temp);
+        }
+
+        // 4. 정렬 후 배열로 변환
+        Collections.sort(answerList);
+        return answerList.toArray(new String[0]);
+
     }
 
     void generateCombinations(String path, String input, int r, int index, List<String> result) {
