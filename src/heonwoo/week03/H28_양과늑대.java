bc528ce4682e558f881c7fb8c7f98c171b4494f0
heonwoo <phwoo1315@naver.com>
2025-04-25 13:00:24 +0900

    3주차 트리 문제 풀이

---
+++ src/heonwoo/week03/H28_양과늑대.java
@@ -0,0 +1,75 @@
+package heonwoo.week03;
+
+import java.util.*;
+
+public class H28_양과늑대 {
+    public static void main(String[] args) {
+        String[] orders = {"ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"};
+        int[] course = {2, 3, 4};
+
+        System.out.println(Arrays.toString(solution(orders, course)));
+    }
+    public static String[] solution(String[] orders, int[] course) {
+
+        ArrayList<String> answerList = new ArrayList<>();
+        ArrayList<String> combinations = new ArrayList<>();
+        Map<String, Integer> comboCount = new HashMap<>();
+
+        // 부분 조합 만들기
+        for (int i = 0; i < course.length; i++) {
+            for (int j = 0; j < orders.length; j++) {
+                // 사전 순서대로 정렬
+                char[] chars = orders[j].toCharArray();
+                Arrays.sort(chars);
+                generateCombinations("",new String(chars) ,course[i],0,combinations);
+            }
+        }
+
+        // 부분 조합이 같은 경우 맵에 저장 , ex)["AC",3]
+        for (String combo : combinations) {
+            comboCount.put(combo, comboCount.getOrDefault(combo, 0) + 1);
+        }
+
+        // 코스별로 가장 많은 것 찾기
+        for (int cour : course) {
+            //가장 많은 것을
+            int max = 0;
+            List<String> temp = new ArrayList<>();
+
+            for (Map.Entry<String, Integer> entry : comboCount.entrySet()) {
+                String combo = entry.getKey();
+                int count = entry.getValue();
+
+                if (combo.length() == cour && count >= 2) {
+                    if (count > max) {
+                        max = count;
+                        temp.clear();
+                        temp.add(combo);
+                    } else if (count == max) {
+                        temp.add(combo);
+                    }
+                }
+            }
+
+            answerList.addAll(temp);
+        }
+
+        // 정렬 후 배열로 변환
+        Collections.sort(answerList);
+        return answerList.toArray(new String[0]);
+
+    }
+
+    // 부분조합 만드는 함수
+    //generateCombinations("",new String(chars) ,course[i],0,combinations);
+    public static void generateCombinations(String path, String input, int r, int index, List<String> result) {
+        if (path.length() == r) {
+            result.add(path);
+            return;
+        }
+
+        for (int i = index; i < input.length(); i++) {
+            generateCombinations(path + input.charAt(i), input, r, i + 1, result); //input과 r은 고정
+        }
+    }
+}
---
+++ src/heonwoo/week03/H30_간단한유니온파인드알고리즘구현.java
@@ -0,0 +1,48 @@
+package heonwoo.week03;
+
+import java.util.*;
+
+public class H30_간단한유니온파인드알고리즘구현 {
+    public static void main(String[] args) {
+        int[][] operations = {{0,0,1}, {0,1,2}, {1,1,2}};
+        int k = 3;
+
+        System.out.println(Arrays.toString(solution(k, operations)));
+    }
+    private static int[] parent;
+
+    private static int find(int x) {
+        if (parent[x] == x)
+            return x;
+        // 그렇지 않다면 x의 부모를 찾아서 parent[x]에 저장
+        parent[x] = find(parent[x]);
+        return parent[x];
+    }
+
+    private static void union(int x, int y) {
+        int root1 = find(x);
+        int root2 = find(y);
+        parent[root2] = root1;
+    }
+
+    public static Boolean[] solution(int k, int[][] operations) {
+        // 노드의 수 만큼 배열 생성
+        parent = new int[k];
+        // 처음에는 각 노드가 자기 자신을 부모로 가지도록 초기화
+        for (int i = 0; i < k; i++) {
+            parent[i] = i;
+        }
+
+        ArrayList<Boolean> answer = new ArrayList<>();
+
+        for(int[] op : operations) {
+            if (op[0] == 0) {
+                union(op[1], op[2]);
+            }
+            else {
+                answer.add(find(op[1]) == find(op[2]));
+            }
+        }
+        return answer.toArray(new Boolean[0]);
+    }
+}
