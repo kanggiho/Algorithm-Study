d4041812da654d4f38ca471859098330b843f056
kanggiho <kgh5554@naver.com>
2025-04-25 15:17:41 +0900

    giho

--- src/giho/week03/G27_다단계칫솔판매.java
+++ src/giho/week03/G27_다단계칫솔판매.java
@@ -5,20 +5,37 @@ import java.util.HashMap;
 public class G27_다단계칫솔판매 {
 
     public int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
-        int[] answer = {};
+        int[] answer = new int[enroll.length];
 
-        HashMap<String,String> relation = new HashMap<>();
-
-        // 본인, 추천인 해시맵 등록
+        HashMap<String, String> relation = new HashMap<>();
         for (int i = 0; i < enroll.length; i++) {
             relation.put(enroll[i], referral[i]);
         }
 
+        HashMap<String, Integer> profit = new HashMap<>();
 
+        for (int i = 0; i < seller.length; i++) {
+            String tempSeller = seller[i];
+            int money = amount[i] * 100;
 
+            while (true) {
+                int commission = money / 10;
+                int selfProfit = money - commission;
 
+                profit.put(tempSeller, profit.getOrDefault(tempSeller, 0) + selfProfit);
 
+                if (!relation.containsKey(tempSeller) || relation.get(tempSeller).equals("-") || commission == 0) {
+                    break;
+                }
 
+                tempSeller = relation.get(tempSeller);
+                money = commission;
+            }
+        }
+
+        for (int i = 0; i < enroll.length; i++) {
+            answer[i] = profit.getOrDefault(enroll[i], 0);
+        }
 
         return answer;
     }
