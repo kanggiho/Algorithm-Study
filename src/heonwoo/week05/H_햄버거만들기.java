f905a8946b5e4fa4e822669096d6d20cf344de2d
HeonWoo Park <phwoo1315@naver.com>
2025-05-16 20:32:51 +0900

    5주차 문제 풀이

---
+++ src/heonwoo/week05/H_햄버거만들기.java
@@ -0,0 +1,16 @@
+package heonwoo.week05;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class H_햄버거만들기 {
+//    public int solution(int[] ingredient){
+//        int answer = 0;
+//        // 1은 빵, 2는 야채, 3은 고기
+//        Deque<Integer> stack = new ArrayDeque<Integer>();
+//        for (int i = 0; i < ingredient.length; i++) {
+//            stack.push(ingredient[i]);
+//
+//        }
+//    }
+}
