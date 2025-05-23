a379ae89ca723679474dd253f9c9177e36a362db
HeonWoo Park <phwoo1315@naver.com>
2025-05-23 13:39:37 +0900

    5주차 문제 풀이

--- src/heonwoo/week05/H38_네트워크.java
+++ src/heonwoo/week05/H38_네트워크.java
@@ -1,14 +1,30 @@
 package heonwoo.week05;
 
 public class H38_네트워크 {
+    static int[][] computer;
+    static boolean[] visited;
     public int solution(int n, int[][] computers) {
         int answer = 0;
+        computer = computers;
+        visited = new boolean[n];
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++){
+            if (!visited[i]){
+                dfs(i);
                 answer++;
             }
         }
 
         return answer;
+    }
+
+    private void dfs(int current){
+        visited[current] = true;
+
+        for (int i = 0; i < computer.length; i++){
+            if (computer[current][i] == 1 && !visited[i]){
+                dfs(i);
+            }
+        }
     }
 }
