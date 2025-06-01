1c19e9424a40f147a7eac1b31f657995a3f4988d
Giho Kang <kgh5554@naver.com>
2025-06-02 00:54:43 +0900

    0530

---
+++ src/giho/week05/G49_사라지는발판.java
@@ -0,0 +1,22 @@
+package giho.week05;
+
+import java.util.*;
+
+
+public class G49_사라지는발판 {
+
+    public static int ROW,COL;
+
+
+    public int solution(int[][] board, int[] aloc, int[] bloc) {
+
+        COL = board.length;
+        ROW = board[0].length;
+
+
+
+
+
+        return 1;
+    }
+}
