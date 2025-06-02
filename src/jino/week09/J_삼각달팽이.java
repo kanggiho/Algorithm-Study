21b83df687ee1588909a20b6074fe377701726a5
jin_5 <krt8599@naver.com>
2025-06-02 12:22:31 +0900

    삼각달팽이 문제 풀이

---
+++ src/jino/week09/J_삼각달팽이.java
@@ -0,0 +1,37 @@
+package jino.week09;
+
+public class J_삼각달팽이 {
+    class Solution {
+        private static int total;
+        private static int[] answer;
+        private static int beforeT =0;
+        public int[] solution(int n) {
+            for(int i = 1 ; i <= n ; i++){
+                total += i;
+            }
+            for(int i = 1 ; i < n ; i++){
+                beforeT += i;
+            }
+
+
+            //System.out.println(total);
+            answer = new int[total];
+            int num = 1;
+            int j = 1;
+            for(int i = 0 ; i < total ;){
+                if(i < beforeT)
+                    answer[i] =  num;
+                num++;
+                i = i +j;
+                j = j + 1;
+
+
+            }
+            j = 0;
+            num = n;
+
+
+            return answer;
+        }
+    }
+}
