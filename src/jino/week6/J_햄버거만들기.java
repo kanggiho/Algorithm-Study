f416082efaccd66216d01ab0f371e3f3041c9905
jin_5 <krt8599@naver.com>
2025-06-03 13:55:57 +0900

    오류사항 수정

--- src/jino/week6/J_햄버거만들기.java
+++ src/jino/week6/J_햄버거만들기.java
@@ -1,18 +1,18 @@
-package jino.week6;
-
-import java.util.Deque;
-import java.util.List;
-
-public class J_햄버거만들기 {
-    class Solution {
-        private static List<Integer> myList;
-        private static int answer = 0;
-        private static Deque<Integer> dq;
-        public int solution(int[] ingredient) {
-
-
-            return answer;
-
-        }
-    }
-}
+//package jino.week6;
+//
+//import java.util.Deque;
+//import java.util.List;
+//
+//public class J_햄버거만들기 {
+//    class Solution {
+//        private static List<Integer> myList;
+//        private static int answer = 0;
+//        private static Deque<Integer> dq;
+//        public int solution(int[] ingredient) {
+//
+//
+//            return answer;
+//
+//        }
+//    }
+//}
